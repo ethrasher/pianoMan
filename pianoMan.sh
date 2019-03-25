f2fbb216d38875cf79807366ff6f2fca5a514661
liz14zyt <liz14zyt@gmail.com>
2019-03-25 14:54:42 -0400

    removed shell script since now doing it as part of python program

--- pianoMan.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-pythoncv = '/usr/local/Cellar/python/3.7.2_2/bin/python3'
-scriptPath = '/Users/thrasher_elizabeth/Documents/CMU/Homework/Senior_Spring/Capstone/omr/pianoMan/main.py'
-$python $scriptPath
