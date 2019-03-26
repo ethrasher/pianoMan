891bb3fb1f4a42f55e857850d0ced7c7e425ab8e
liz14zyt <liz14zyt@gmail.com>
2019-03-26 16:43:38 -0400

    Fixed documentation in most of the easier files

--- musicSymbolRecognition.py
+++ musicSymbolRecognition.py
@@ -1,14 +1,14 @@
 #Will find and remove staffs
-#Will characterize notes
+#Will characterize notes and organize to write to xml
 
 from staffLineDetectionRemoval import staffLineDetectionRemoval
 from segmentationAndRecognition import segmentationAndRecognition
 
 def musicSymbolRecognition(binaryImg):
-    # DESCRIPTION: finds and removes all the staff lines in the original image
+    # DESCRIPTION: Takes the binary image and finds and recognizes all notes to be written to xml
     # PARAMETERS: binaryImg: numpy array of the binarized image (255 or 0 in all places)
-    # RETURN: staffLineRows: a 1D numpy array containing the number of black pixels in each row
-    print("in musicSymbolRecognition")
+    # RETURN: allMeasures: a list of lists of ConnectedComponent objects to be written to the xml
+
     newBinaryImg, staffLineRows = staffLineDetectionRemoval(binaryImg= binaryImg)
     allMeasures = segmentationAndRecognition(binaryImg=binaryImg, staffLines=staffLineRows)
     return allMeasures
--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,6 @@
 #File to handle the preprocessing of the music sheet
 #Fairly limited because we are assuming we are getting an ideal scan
+#Could add more for noise reduction or handling some jpg compression issues if need be
 
 import cv2
 
@@ -8,14 +9,15 @@ def preprocess(path):
     # May handle other issues as well as less ideal scans come up
     # PARAMETERS: path: string of the full path to the image (jpg)
     # RETURN: numpy array of the binarized image (255 or 0 in all pixels)
+
     binaryImg = binarizeImage(path)
-    showBinaryImage(binaryImg=binaryImg)
     return binaryImg
 
 def binarizeImage(path):
     #DESCRIPTION: turns a jpg into a binarized version of the image as a numpy 2D array
     #PARAMETERS: path: string of the full path to the image (jpg)
     #RETURN: numpy array of the binarized image (255 or 0 in all pixels)
+
     img = cv2.imread(path, 0) #Load an color image in grayscale of the page of music
     ret,binaryImg = cv2.threshold(img,230,255,cv2.THRESH_BINARY)
     return binaryImg
@@ -26,10 +28,7 @@ def binarizeImage(path):
 def showBinaryImage(binaryImg):
     #DESCRIPTION: opens a new window and displays the image
     #PARAMETERS: binaryImage: numpy array of the binarized image (255 or 0 in all pixels)
-    #RETURN: void
-    #cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-    #cv2.resizeWindow('image', 400, 600)
+    #RETURN: None
+
     cv2.imshow('image',binaryImg)
     cv2.waitKey(0)
-    #cv2.destroyAllWindows()
-    print("end")
